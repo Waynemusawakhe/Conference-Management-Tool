37b3684034b623ddf9cbdb47fe65ab973b343f84
BoitumeloMoile <99957790+BoitumeloMoile@users.noreply.github.com>
2026-08-19 17:40:25 +0200

    Import Login and register

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,9 @@
 import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
 
+<Route path="/login" element={<Login />} />
+<Route path="/register" element={<Register />} />
 export default function App() {
   return <Home />;
 }
