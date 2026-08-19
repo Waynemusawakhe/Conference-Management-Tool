84972cb3593fa152526a4ae49c5660b0a0838da2
BoitumeloMoile <99957790+BoitumeloMoile@users.noreply.github.com>
2026-08-19 17:45:24 +0200

    Refactor App component to use Routes for navigation

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,8 +3,12 @@ import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 
-<Route path="/login" element={<Login />} />
-<Route path="/register" element={<Register />} />
 export default function App() {
-  return <Home />;
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/register" element={<Register />} />
+    </Routes>
+  );
 }
