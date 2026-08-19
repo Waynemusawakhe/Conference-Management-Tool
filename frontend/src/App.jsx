36fb37ac078c46e70a700ee9171c71a1643c0f13
Zzanele Radebe <zaneradebe6@gmail.com>
2026-08-19 11:32:53 +0200

    Add Contact page with validation and wire up React Router navigation

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,14 @@
+import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
+import Contact from "./pages/Contact";
+import HelpFAQ from "./pages/HelpFAQ";
 
 export default function App() {
-  return <Home />;
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/contact" element={<Contact />} />
+      <Route path="/help-faq" element={<HelpFAQ />} />
+    </Routes>
+  );
 }
