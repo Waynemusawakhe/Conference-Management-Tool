196f5c62698e486c6117237b45d3dfc42daa7f96
Vinolia Marumo <marumothulaganyo@gmail.com>
2026-08-20 16:00:44 +0200

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,7 @@
 import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
-//import Register from "./pages/Register";
-//import Login from "./pages/Login";
+import Register from "./pages/Register";
+import Login from "./pages/Login";
 import Contact from "./pages/Contact";
 import HelpFAQ from "./pages/HelpFAQ";
 import About from "./pages/About";
@@ -10,7 +10,7 @@ export default function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
-      {/*<Route path="/login" element={<Login />} /> */}
+      <Route path="/login" element={<Login />} /> 
       {/*<Route path="/register" element={<Register />} /> */}
       <Route path="/contact" element={<Contact />} />
       <Route path="/help-faq" element={<HelpFAQ />} />
