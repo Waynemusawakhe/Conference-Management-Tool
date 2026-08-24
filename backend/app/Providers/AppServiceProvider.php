e8d590164d02b39f4b675b2fe64c4dd7af1222d2
41062817 <131959847+41062817@users.noreply.github.com>
2026-08-24 23:27:03 +0200

    Merge remote changes before pushing composer.lock update

--- backend/app/Providers/AppServiceProvider.php
+++ backend/app/Providers/AppServiceProvider.php
@@ -3,6 +3,10 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use App\Models\User;
+use App\Modules\Conferences\Models\Conference;
+use App\Modules\Conferences\Policies\ConferencePolicy;
+use Illuminate\Support\Facades\Gate;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +23,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        Gate::policy(Conference::class, ConferencePolicy::class);
     }
 }
